f81fe73bb5cc5b8287fe28220176bba207fcff90
Nikhil Tajanpure <niktaj222@gmail.com>
2025-05-27 22:21:11 +0100

    First Commit Section 1

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(DataContext context) : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await context.AppUsers.ToListAsync();
+            return users;
+        }
+
+
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user =  await context.AppUsers.FindAsync(id);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            return user;
+        }
+
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace WebApplication1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
